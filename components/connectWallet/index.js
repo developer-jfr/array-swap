2fddf136cb7980b042c477b48c3e9c2363b7fd5b
frdevelopergen <erkinbaevbakai8@gmail.com>
2022-10-23 13:30:51 +0600

    refactor code

--- components/connectWallet/index.js
+++ components/connectWallet/index.js
@@ -1,58 +1,64 @@
 import Image from "next/image";
-import { useAccount, useConnect, useDisconnect  } from "wagmi";
-import classes from './index.module.scss';
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import classes from "./index.module.scss";
 
 const Close = "/assets/images/icons/hix.svg";
 const Metamask = "/assets/images/svg/metamask.svg";
 const ConnectWalletIcon = "/assets/images/svg/walletconnect-logo.svg";
-const Coinbase = '/assets/images/icons/coinbase.svg';
+const Coinbase = "/assets/images/icons/coinbase.svg";
 
 const ConnectWallet = ({ setOpenWallet, openWallet }) => {
-  const { address, connector, isConnected,status } = useAccount();
-  const { connect, connectors, error,isError } =
-    useConnect();
-
-
-
-
+  const { address, connector, isConnected, status } = useAccount();
+  const { connect, connectors, error, isError } = useConnect();
 
   return (
-    <div class={`modal ${isConnected && openWallet ? "" : "is-active"} ${classes.modal}`}>
+    <div
+      class={`modal ${isConnected && openWallet ? "" : "is-active"} ${
+        classes.modal
+      }`}
+    >
       <div
         class="modal-background"
         style={{ background: "rgba(50, 53, 70, 0.6)" }}
         onClick={() => setOpenWallet(false)}
       ></div>
       <div class={`modal-card ${classes.modalCard}`}>
-        <div className={`is-flex is-justify-content-end p-5 is-clickable ${classes.modalHeader}`}>
+        <div
+          className={`is-flex is-justify-content-end p-5 is-clickable ${classes.modalHeader}`}
+        >
           <Image
             src={Close}
             height={20}
             width={20}
             onClick={() => setOpenWallet(false)}
-            alt='cool'
+            alt="cool"
           />
         </div>
         <section class="modal-card-body">
           <div className={classes.itemsWrapp}>
-            <a onClick={() => {
-              connect({ connector: connectors[0] })
-              console.log(error)
-              setOpenWallet(false)
-              
-            }}>
+            <a
+              onClick={() => {
+                connect({ connector: connectors[0] });
+                console.log(error);
+                setOpenWallet(false);
+              }}
+            >
               <img src={Metamask} alt="" />
             </a>
-            <a onClick={() => {
-              connect({ connector: connectors[1] })
-              setOpenWallet(false)
-              }}>
+            <a
+              onClick={() => {
+                connect({ connector: connectors[1] });
+                setOpenWallet(false);
+              }}
+            >
               <img src={ConnectWalletIcon} alt="" />
             </a>
-            <a onClick={() => {
-              connect({ connector: connectors[2] })
-              setOpenWallet(false)
-              }}>
+            <a
+              onClick={() => {
+                connect({ connector: connectors[2] });
+                setOpenWallet(false);
+              }}
+            >
               <img src={Coinbase} alt="" />
             </a>
           </div>
