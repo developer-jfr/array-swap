98c1ef2687c5f773cabe7ce059dff82a73239cba
frdevelopergen <erkinbaevbakai8@gmail.com>
2022-12-12 09:31:52 +0600

    [FEATURE-12]-changed toastify-alert

--- components/toastify-alert/index.js
+++ components/toastify-alert/index.js
@@ -1,9 +1,6 @@
-import React, { useEffect } from "react";
-
+import { useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-
 
 function ToastifyAlert({ text}) {
   useEffect(() => {
